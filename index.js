9e82cf2af20354eb863710261cab93d984bd1108
ricardo197578 <richardlihue@gmail.com>
2023-06-21 00:10:13 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+const app = express();
+
+//Conectar a la base de datos
+db.authenticate()
+	.then(()=>console.log('BD CONECTADA'))
+	.catch(error=> console.log('error'));
+
+
+//definir puerto
+const port = process.env.PORT || 4000;
+
+//Habilitar Pug
+app.set('view engine','pug');
+
+//Obtener año actual
+app.use((req, res, next) => {
+	const year = new Date();
+
+	res.locals.actualYear = year.getFullYear();
+	res.locals.nombreSitio= "Agencia de Viajes";
+	return next();
+
+});
+
+//Agregar body parser para leer los datos del formulario
+
+app.use(express.urlencoded({extended:true}));
+
+//Definir carpeta public
+app.use(express.static('public'));
+
+//agregar router
+app.use('/',router);
+
+app.listen(port, ()=>{
+	console.log(`SERVIDOR OK PUERT ${port}`)
+})
