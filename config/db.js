17f91ac78756471e0e462ae1a6299a09c4329587
ricardo197578 <richardlihue@gmail.com>
2023-07-07 15:44:14 -0300

    cambio config db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv/config'
 
 console.log(process.env.DB_HOST)
 
-//const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-const db = new Sequelize('metalurgicaweb','root','celina2015',{
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+
 	host:process.env.DB_HOST,
 	port:'3306',
 	dialect:'mysql',
